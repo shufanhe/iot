ce1f70544a5eb0537618cf4b7737620de5140291
StevenReiss <spr@cs.brown.edu>
2022-12-28 09:47:38 -0500

    Initial smartapp implementation in cedes

--- cedes/smartthings.js
+++ cedes/smartthings.js
@@ -8,10 +8,13 @@
 /*										*/
 /********************************************************************************/
 "use strict";
-
-
+  
+const SmartApp = require('@smartthings/smartapp');
+const FileContextStore = require('@smartthings/file-context-store')  
+      
 const config = require("./config");
 
+var smartapp;
 
 
 
@@ -23,7 +26,11 @@ const config = require("./config");
 
 function getRouter(restful)
 {
+   setupSmartApp();
+   
    restful.use(authenticate);
+   
+   restful.post("/smartthings",handleSmartThings);
 
    restful.all("*",config.handle404)
    restful.use(config.handleError);
@@ -50,6 +57,71 @@ function addBridge(authdata)
 {
    return false;
 }
+
+
+/********************************************************************************/
+/*                                                                              */
+/*      Setup smart app                                                         */
+/*                                                                              */
+/********************************************************************************/
+
+function setupSmartApp()
+{
+   smartapp = new SmartApp()
+      .enableEventLogging(2)
+//    .clientSecret("xxx")
+//    .clientId("xxxx")
+//    .publicKey('xxxxx')
+      .page('mainPage',mainPage)
+      .permissions(['r:devices:*','w:devices:*','x:devices:*'])
+      .updated(handleSmartThingsUpdates)
+      .subscribedEventHandler(handleSmartThingsEvents)
+      .contextStore(new FileContextStore());
+}
+
+
+function mainPage(context,page,configData)
+{
+}
+
+
+async function handleSmartThingsUpdates(context,data)
+{
+   console.log("Update/Install",context,data)
+   await context.api.subscriptions.delete();
+}
+
+
+async function handleSmartThingsEvents(context,event)
+{
+   console.log("Event",context,event);
+}
+
+
+
+/********************************************************************************/
+/*                                                                              */
+/*      Smart things interactions                                               */
+/*                                                                              */
+/********************************************************************************/
+
+async function handleSmartThings(req,res)
+{
+   console.log("SMARTTHINGS",req.body);
+   
+   await smartapp.handleHttpCallback(req,res);
+}
+
+
+async function handleSmartThingsGet(req,res)
+{
+   console.log("SMARTTHINGS-GET",req.query);
+   
+   req.body = req.query;
+   
+   handleSmartThings(req,res);
+}
+
 
 
 /********************************************************************************/
